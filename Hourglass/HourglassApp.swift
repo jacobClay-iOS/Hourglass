0ab3d21c787ae61f3a7303e088f63e3b385e7183
Jacob <jclay34@hotmail.com>
2024-10-21 19:12:10 -0500

    Initial Commit

---
+++ Hourglass/HourglassApp.swift
@@ -0,0 +1,17 @@
+//
+//  HourglassApp.swift
+//  Hourglass
+//
+//  Created by Jacob Clay on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct HourglassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
